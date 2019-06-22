00bc5b8127a38f790099b1da89fd1cca7af02170
Roman Bednarz <roman.bednarz@gmail.com>
2019-06-23 00:49:17 +0200

    done contact and contactform

---
+++ components/Contact.js
@@ -0,0 +1,49 @@
+/*
+
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement("div", {className: "contactItem"},
+        React.createElement("img", {
+          className: "contactImage", 
+          src: "http://icons.iconarchive.com/icons/graphicloads/100-flat-2/256/contacts-icon.png"
+        }),
+        React.createElement("p", {className: "contactLabel"}, "Imię: " + this.props.item.firstName),
+        React.createElement("p", {className: "contactLabel"}, "Nazwisko: " + this.props.item.lastName),
+        React.createElement("a", {className: "contactEmail", href: "mailto:" + this.props.item.email}, 
+          this.props.item.email
+        )
+      )
+    )
+  },
+});
+
+*/
+
+
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      <div className={'contactItem'}>
+        <img className={'contactImage'} src={'http://icons.iconarchive.com/icons/graphicloads/100-flat-2/256/contacts-icon.png'}/>
+        <p className={'contactLabel'}>
+          Imię: {this.props.item.firstName}
+        </p>
+        <p className={'contactLabel'}>
+          Nazwisko: {this.props.item.lastName}
+        </p>
+        <a className={'contactEmail'} href={'mailto' + this.props.item.email}
+          {this.props.item.email}
+        </a>
+      </div>
+    )
+  }
+});
---
+++ components/ContactForm.js
@@ -0,0 +1,49 @@
+/*
+
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+
+  render: function() {
+    return (
+      React.createElement("form", {className: "contactForm"},
+        React.createElement("input", {
+          type: "text",
+          placeholder: "Imię",
+          value: this.props.contact.firstName,
+        }),
+        React.createElement("input", {
+          type: "text",
+          placeholder: "Nazwisko",
+          value: this.props.contact.lastName,
+        }),
+        React.createElement("input", {
+          type: "email",
+          placeholder: "Email",
+          value: this.props.contact.email,
+        }),
+        React.createElement("button", {type: "submit"}, "Dodaj kontakt")
+      )
+    )
+  },
+})
+
+*/
+
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+
+  render: function() {
+    return (
+      <form className={'contactForm'}>
+        <input type='text' placeholder='Imię' value={this.props.contact.firstName}></input>
+        <input type='text' placeholder='Nazwisko' value={this.props.contact.lastName}></input>
+        <input type='email' placeholder='Email' value={this.props.contact.email}></input>
+        <button type='submit'>Dodaj kontakt</button>
+      </form>  
+    )
+  },
+});
